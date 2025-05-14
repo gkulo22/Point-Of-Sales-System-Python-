012aaac0acc58ef7fbc6378b850f55d0673725b9
gkulo22 <147552516+gkulo22@users.noreply.github.com>
2025-05-14 20:37:46 +0400

    Update __main__.py

--- app/runner/__main__.py
+++ app/runner/__main__.py
@@ -4,3 +4,5 @@ from app.runner.setup import setup
 
 if __name__ == '__main__':
     UvicornServer.from_env().run(setup())
+
+// or run this command in terminal: uvicorn app.runner.asgi:app --reload
